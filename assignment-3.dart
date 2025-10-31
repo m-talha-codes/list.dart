7f2a0e523f81bb9519398babbb4f05d89a54e81a
m-talha-codes <itztalha05@gmail.com>
2025-11-01 02:42:01 +0500

    first commit

--- assignment-3.dart
+++ assignment-3.dart
@@ -27,10 +27,19 @@ void main() {
   //Q-4
   List number = [12, 8, 30, 4, 53, 61, 7, 82, 93, 10];
   number.sort();
-  print('Smallest: ${number.first} \nGreatest: ${number.last}');
+  print('Smallest: ${number.first}');
+  print('Greatest: ${number.last}');
+
 
   //Q-5
-  List intList = [22, 12, 33, 24, 45, 36, 77, 28, 29, 30];
+  List intList = [22, 12, 33, 24, 30];
   var maxValue = intList.reduce((a, b) => a > b ? a : b);
   print('Max Value: $maxValue');
+
+  // Q-6
+  List oldList = ['Apple', 'Mango', 'Watermelon'];
+
+  List newList = List.from(oldList.reversed);
+  print('New List: $newList');
+  print('Old List: $oldList');
 }
